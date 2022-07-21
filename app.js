2ec8b2efa6e26e11e28dbaec5eaef05241c4a5c8
ken0gul <ken0gul@gmail.com>
2022-07-21 19:27:00 -0400

    Add basic functionality to addBook and display them

--- app.js
+++ app.js
@@ -1,27 +1,65 @@
 // Selecting elements
+const inputTitle = document.querySelector('#title');
+const inputAuthor = document.querySelector('#author');
+const inputPages = document.querySelector('#pages');
+const inputIsRead = document.querySelector('#read');
+const addBtn = document.querySelector('.add-button > button');
+const container = document.querySelector('.container');
 
-// Creating the first book or the array
-let myLibrary = [
-    {
-        title: 'Harry Potter',
-        author: "J.K Rownling",
-        pages: 980,
-        isRead: true
-
-    }
 
 
 
-]
+// Creating the first book or the array
+let myLibrary = [];
 
-const card =`
-    <div class="card">
-    <span>Title: ${myLibrary[0].title}</span>
-    <span>Title: ${myLibrary[0].author}</span>
-    <span>Title: ${myLibrary[0].pages}</span>
-    <span>Title: ${myLibrary[0].isRead}</span>
-    
-    </div>
-`
+// Constructor function to create book instances
+function Book(title,author,pages,isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+}
 
-document.body.insertAdjacentHTML("beforebegin", card);
+// Function to take user's input and create an instance then store it to myLibrary array
+
+function addBook() {
+    let newBook = new Book(inputTitle.value,inputAuthor.value,inputPages.value,inputIsRead.value);
+    myLibrary.push(newBook)
+    displayBooks()
+    console.log(myLibrary)
+    return newBook
+}
+
+
+function displayBooks() {
+    let card;
+    let id = new Date().getTime();
+    myLibrary.forEach(book => {
+        console.log(book.title)
+         card =`
+        <div class="card" data-id="${id}">
+        
+        <span>Title: ${book.title}</span>
+        <span>Author: ${book.author}</span>
+        <span>Pages: ${book.pages}</span>
+        <span>Read: ${book.isRead}</span>
+        
+        </div>
+        `
+    })
+
+document.body.insertAdjacentHTML("afterbegin", card);
+
+// Let's select that card..
+const cardID =document.querySelector(`div[data-id="${id}"]`);
+cardID.style.display = "flex";
+container.style.display ='none';
+
+}
+
+
+
+
+
+// Button functionality
+addBtn.addEventListener('click', addBook)
