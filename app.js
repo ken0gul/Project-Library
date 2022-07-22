6bb8e748607b525d9f5ef4652f9b47d53ae454c4
ken0gul <ken0gul@gmail.com>
2022-07-21 22:07:10 -0400

    Add restriction for invalid input

--- app.js
+++ app.js
@@ -22,38 +22,61 @@ function Book(title,author,pages,isRead) {
 
 // Function to take user's input and create an instance then store it to myLibrary array
 
-function addBook() {
+function addBook(e) {
+    if(myLibrary.length >= 8) return;
+
+    if(inputTitle.value === "" || inputAuthor.value === "" || inputPages.value === "" || inputIsRead.value === "") return; 
+    
     let newBook = new Book(inputTitle.value,inputAuthor.value,inputPages.value,inputIsRead.value);
     myLibrary.push(newBook)
     displayBooks()
-    console.log(myLibrary)
+    inputTitle.value = "";
+    inputAuthor.value = "";
+    inputPages.value = "";
+    inputIsRead.value = "";
+
+
     return newBook
 }
-
+const wrapper = `<div class="wrapper"></div>`
+    document.body.insertAdjacentHTML('afterbegin', wrapper)
 
 function displayBooks() {
     let card;
     let id = new Date().getTime();
+
+    
     myLibrary.forEach(book => {
-        console.log(book.title)
          card =`
+         
         <div class="card" data-id="${id}">
-        
-        <span>Title: ${book.title}</span>
-        <span>Author: ${book.author}</span>
-        <span>Pages: ${book.pages}</span>
-        <span>Read: ${book.isRead}</span>
-        
+        <div class="single-item">
+        <span class="single_item_title">Title</span>
+        <span>${book.title}</span>
+        </div>
+        <div class="single-item">
+        <span class="single_item_title">Author</span>
+        <span>${book.author}</span>
+        </div>
+        <div class="single-item">
+        <span class="single_item_title">Pages</span>
+        <span>${book.pages}</span>
+        </div>
+        <div class="single-item">
+        <span class="single_item_title">Read</span>
+        <span>${book.isRead}</span>
+        </div>
         </div>
         `
     })
 
-document.body.insertAdjacentHTML("afterbegin", card);
+document.querySelector('.wrapper').insertAdjacentHTML("afterbegin", card);
+document.body.style.justifyContent = 'space-around'
 
 // Let's select that card..
 const cardID =document.querySelector(`div[data-id="${id}"]`);
 cardID.style.display = "flex";
-container.style.display ='none';
+// container.style.display ='none';
 
 }
 
